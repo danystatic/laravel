e4a4d53799d2f44b51fdc4831e9b9916a889941f
Daniel Villarreal <danielvt@gmail.com>
2016-09-16 05:03:43 -0500

    simple change eloquent

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -117,7 +117,7 @@ class ApiController extends Controller
 
     	$feedback = new Feedback;
     	$feedback->json 	=$request->json;
-    	$feedback->key 		=$request->'video-1';
+    	$feedback->key 		=$request->key;
     	$feedback->value 	=$request->value;
     	$feedback->save();
     	return response()->json(['res'=>"success"]);
