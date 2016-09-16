bf88d62bb3d776177f5e099336b4aef3f01e6bb6
Daniel Villarreal <danielvt@gmail.com>
2016-09-16 05:05:32 -0500

    nothing wow

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -117,7 +117,7 @@ class ApiController extends Controller
 
     	$feedback = new Feedback;
     	$feedback->json 	=$request->json;
-    	$feedback->key 		=$request->key;
+    	//$feedback->key 		=$request->key;
     	$feedback->value 	=$request->value;
     	$feedback->save();
     	return response()->json(['res'=>"success"]);
